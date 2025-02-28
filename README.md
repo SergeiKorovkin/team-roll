d3cf9f6e05400072945e8923fb0894eadc4550b7
SergeiKorovkin <98321980+SergeiKorovkin@users.noreply.github.com>
2025-02-28 04:16:43 -0800

    update builds

--- README.md
+++ README.md
@@ -16,12 +16,12 @@ before need install docker
 
 
 для настройки https нужно добавить сертификаты (временное решение)
-1. запустить сервер и запустить команду sudo certbot renew --force-renewal
+1. запустить сервер и запустить команду sudo certbot renew --force-renewal использовать команду npm run server и поменять url для mongodb в configs
 2. создать папку docker-certs     mkdir -p ~/docker-certs
 3. скопировать сертификаты в папку docker-certs
- - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/fullchain.pem ~/docker-certs/
- - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/chain.pem ~/docker-certs/
- - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/cert.pem ~/docker-certs/
- - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/privkey.pem ~/docker-certs/
+ - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/fullchain7.pem ~/docker-certs/
+ - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/chain7.pem ~/docker-certs/
+ - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/cert7.pem ~/docker-certs/
+ - sudo cp /etc/letsencrypt/archive/lmru-returns.ru/privkey7.pem ~/docker-certs/
 4. дать доступ на чтение файла privkey  sudo chmod 644 privkey.pem
 5. перезапустить сервер
--- dist/app.js
+++ dist/app.js
@@ -1,64 +1,65 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const config_1 = __importDefault(require("config"));
-const https_1 = __importDefault(require("https"));
-const fs_1 = __importDefault(require("fs"));
-const auth_routes_1 = __importDefault(require("./src/routes/auth.routes"));
-const user_routes_1 = __importDefault(require("./src/routes/user.routes"));
-const bets_routes_1 = __importDefault(require("./src/routes/bets.routes"));
-const path_1 = __importDefault(require("path"));
-const mongoose = require('mongoose');
-const app = (0, express_1.default)();
-let http = false;
-let httpsOptions = {};
+'use strict'
+var __importDefault =
+	(this && this.__importDefault) ||
+	function (mod) {
+		return mod && mod.__esModule ? mod : { default: mod }
+	}
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+const config_1 = __importDefault(require('config'))
+const https_1 = __importDefault(require('https'))
+const fs_1 = __importDefault(require('fs'))
+const auth_routes_1 = __importDefault(require('./src/routes/auth.routes'))
+const user_routes_1 = __importDefault(require('./src/routes/user.routes'))
+const bets_routes_1 = __importDefault(require('./src/routes/bets.routes'))
+const path_1 = __importDefault(require('path'))
+const mongoose = require('mongoose')
+const app = (0, express_1.default)()
+let http = true
+let httpsOptions = {}
 try {
-    httpsOptions = {
-        key: fs_1.default.readFileSync('/etc/letsencrypt/live/lmru-returns.ru/privkey.pem'),
-        cert: fs_1.default.readFileSync('/etc/letsencrypt/live/lmru-returns.ru/fullchain.pem'),
-    };
+	httpsOptions = {
+		key: fs_1.default.readFileSync('/etc/letsencrypt/live/lmru-returns.ru/privkey.pem'),
+		cert: fs_1.default.readFileSync('/etc/letsencrypt/live/lmru-returns.ru/fullchain.pem'),
+	}
+} catch (e) {
+	http = true
 }
-catch (e) {
-    http = true;
-}
-const isProd = process.env.NODE_ENV === 'production';
-app.use(express_1.default.json({ limit: '3mb' }));
-app.use('/api/user', user_routes_1.default);
-app.use('/api/auth', auth_routes_1.default);
-app.use('/api/bets', bets_routes_1.default);
+const isProd = process.env.NODE_ENV === 'production'
+app.use(express_1.default.json({ limit: '3mb' }))
+app.use('/api/user', user_routes_1.default)
+app.use('/api/auth', auth_routes_1.default)
+app.use('/api/bets', bets_routes_1.default)
 // Настройте express для обслуживания файлов из .well-known/acme-challenge для https
-app.use('/.well-known/acme-challenge', express_1.default.static(path_1.default.join(__dirname, '.well-known', 'acme-challenge')));
+app.use(
+	'/.well-known/acme-challenge',
+	express_1.default.static(path_1.default.join(__dirname, '.well-known', 'acme-challenge'))
+)
 if (isProd) {
-    app.use('/', express_1.default.static(path_1.default.join(__dirname, '..', 'client', 'build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path_1.default.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-    });
+	app.use('/', express_1.default.static(path_1.default.join(__dirname, '..', 'client', 'build')))
+	app.get('*', (req, res) => {
+		res.sendFile(path_1.default.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+	})
 }
-const PORT = http ? 80 : config_1.default.get('port') || 5005;
+const PORT = http ? 5001 : config_1.default.get('port') || 5005
 async function start() {
-    try {
-        await mongoose.connect(config_1.default.get('mongoURL'));
-        if (isProd) {
-            if (http) {
-                app.listen(PORT, () => console.log(`App has been started on port  ${PORT}...`));
-            }
-            else {
-                https_1.default.createServer(httpsOptions, app).listen(PORT, () => {
-                    console.log(`HTTPS server started on port ${PORT}...`);
-                });
-            }
-        }
-        else {
-            app.listen(PORT, () => console.log(`App has been started on port  ${PORT}...`));
-        }
-    }
-    catch (e) {
-        console.log('Server error: ', e.message);
-        process.exit(1);
-    }
+	try {
+		await mongoose.connect(config_1.default.get('mongoURL'))
+		if (isProd) {
+			if (http) {
+				app.listen(PORT, () => console.log(`App has been started on port  ${PORT}...`))
+			} else {
+				https_1.default.createServer(httpsOptions, app).listen(PORT, () => {
+					console.log(`HTTPS server started on port ${PORT}...`)
+				})
+			}
+		} else {
+			app.listen(PORT, () => console.log(`App has been started on port  ${PORT}...`))
+		}
+	} catch (e) {
+		console.log('Server error: ', e.message)
+		process.exit(1)
+	}
 }
-start();
+start()
 //# sourceMappingURL=app.js.map
